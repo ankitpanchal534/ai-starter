c60532b0bf509d73c0dbe2f1ee12598a27639112
Ankit Panchal <31965758+ankitpanchal534@users.noreply.github.com>
2024-07-21 23:04:43 +0530

    sas

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,7 +1,7 @@
  const handler=(req,res) =>{
 
 console.log(req);
-return res.status(200).json({text:'i am working!',req.body})
+return res.status(200).json({text:'i am working!',data:req.body})
 }
 
 

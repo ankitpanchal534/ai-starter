ce88d53bfb2a17ed4982d579753bd2ba40ec30e7
Ankit Panchal <31965758+ankitpanchal534@users.noreply.github.com>
2024-07-21 23:13:32 +0530

    added api

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,8 +1,59 @@
- const handler=(req,res) =>{
+import nodemailer from 'nodemailer';
 
-console.log(req);
-return res.status(200).json({text:'i am working!',data:req.body})
-}
+const handler = (req, res) => {
+    if (req.method === 'POST') {
+        // Get the data from the request body
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk.toString();
+        });
+
+        req.on('end', () => {
+            try {
+                // Parse the JSON data
+                const { message, email, phoneNumber, name, offerAmount } = JSON.parse(data);
+                // console.log('data', data)
+                // Create a nodemailer transporter
+                let transporter = nodemailer.createTransport({
+                    // Configure your email provider here
+                    service: 'gmail',
+                    auth: {
+                        user: 'apdudhla9837@gmail.com',
+                        pass: '9837391864'
+                    }
+                });
 
 
- export default handler;
+                // Setup email data
+                let mailOptions = {
+                    from: 'apdudhla9837@gmail.com',
+                    to: 'ankitpanchal534@gmail.com',
+                    subject: 'Domain Enquire',
+                    text: data
+                };
+
+                // Send email
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.error('Error sending email:', error);
+                        res.statusCode = 500;
+                        res.end('Failed to send email');
+                    } else {
+                        console.log('Email sent:', info.response);
+                        res.statusCode = 200;
+                        res.end('Email sent successfully');
+                    }
+                });
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                res.statusCode = 400;
+                res.end('Invalid JSON data');
+            }
+        });
+    } else {
+        res.statusCode = 405;
+        res.end('Method Not Allowed');
+    }
+};
+
+export default handler;
